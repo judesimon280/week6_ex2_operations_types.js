759408963e425db4cb3b1057b92d6146b30466ea
judesimon280 <j18841361@gmail.com>
2026-05-08 18:57:45 +0200

    Add files via upload

---
+++ javascript exercise 2 .js
@@ -0,0 +1,16 @@
+
+let number1 = 10;
+
+let number2 = 6;
+
+let number1String = number1.toString();
+
+let sum = 10 + 6;
+
+let product = 10* 6;
+
+console.log("Number 1:", 10);
+console.log("Number 2:", 6);
+console.log("Number 1 as a string:", number1String);
+console.log("sum:", 16);
+console.log("Product:", 60);
